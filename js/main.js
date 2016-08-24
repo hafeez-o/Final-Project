f13fc66e210155b386b5397ef40788e03bf26fd6
Hafeez Oyetunde <hafeez1918@outlook.com>
2016-08-24 11:31:29 -0400

    I added some new css and javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-alert("North America is a continent that has a lot of experience but little accomplishment and with only three countries the competition is stiff. You can click okay to find out more about these three countries");
+/*function myFunction() {
+    var x = document.getElementsByClassName('myDIV');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+} */
+function myFunction() {
+    myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
